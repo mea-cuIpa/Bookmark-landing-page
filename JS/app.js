7e4abd34c97f15a21b9aa0504f5d5d0dc6efb992
mea-cuIpa <juchnevic98@outlook.com>
2021-02-08 18:53:04 +0200

    features section fix

--- JS/app.js
+++ JS/app.js
@@ -34,21 +34,21 @@ let features = [
     heading: 'Bookmark in one click',
     description:
       'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.',
-    imgPath: './img/illustration-features-tab-1.svg',
+    imgPath: './images/illustration-features-tab-1.svg',
     altText: 'dashboard',
   },
   {
     heading: 'Intelligent search',
     description:
       'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
-    imgPath: './img/illustration-features-tab-2.svg',
+    imgPath: './images/illustration-features-tab-2.svg',
     altText: 'dashboard with magnifying glass',
   },
   {
     heading: 'Share your bookmarks',
     description:
       'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.',
-    imgPath: './img/illustration-features-tab-3.svg',
+    imgPath: './images/illustration-features-tab-3.svg',
     altText: 'people waving to each other',
   },
 ];
